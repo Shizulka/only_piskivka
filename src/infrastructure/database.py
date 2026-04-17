32593e9a0f7ebfa5a5e7b3c4854ad92c92c917fb
tamanegich <tamanegichu@tuta.io>
2026-04-17 19:24:22 +0300

    change stuff to use env instead of showing of our passwords

--- src/infrastructure/database.py
+++ src/infrastructure/database.py
@@ -1,10 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 from src.infrastructure.models import Base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:1@127.0.0.1:5432/postgres"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
--- src/infrastructure/main.py
+++ src/infrastructure/main.py
@@ -1,26 +1,25 @@
+import os
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import create_engine, text
 from src.infrastructure.database import get_db, db_ping
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:1@127.0.0.1:5432/postgres"
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def db_ping(db: Session) -> None:
-    db.execute(text("SELECT 1"))
 
 @app.get("/health")
 def health_check(db: Session = Depends(get_db)):
